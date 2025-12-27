d80cf84dec00de4c4c5fafcceec7534e5febe076
Rikka Botan <168765697+Rikka-Botan@users.noreply.github.com>
2025-12-28 07:05:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 console.log("Portfolio loaded");
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
 const targets = document.querySelectorAll(
     "section, .project, .qiita-card, .repo-card, .hf-card, .activity-card"
   );
@@ -14,6 +21,7 @@ const targets = document.querySelectorAll(
         if (entry.isIntersecting) {
           entry.target.classList.add("is-visible");
   
+          // 表示後の演出
           entry.target.classList.add("sway");
   
           observer.unobserve(entry.target);
