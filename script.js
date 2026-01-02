847fe6d02e1c6a090303dc99350b4a34d3f02e6f
Rikka Botan <168765697+Rikka-Botan@users.noreply.github.com>
2026-01-03 01:00:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -31,4 +31,44 @@ const targets = document.querySelectorAll(
   );
   
   targets.forEach(el => observer.observe(el));
+
+  const flowers = ["🩵", "❄️", "❄️", "❄️", "❄️", "❄️", "❄️", "❄️", "❄️", "❄️"];
+    
+  function createFlower() {
+    const flower = document.createElement("div");
+    flower.className = "falling-flower";
+    flower.textContent = flowers[Math.floor(Math.random() * flowers.length)];
   
+    const size = 24 + Math.random() * 32;
+    const duration = 3 + Math.random() * 6;
+  
+    flower.style.left = Math.random() * window.innerWidth + "px";
+    flower.style.fontSize = size + "px";
+    flower.style.animationDuration = `${duration}s, ${3 + Math.random() * 4}s`;
+  
+    document.body.appendChild(flower);
+  
+    setTimeout(() => {
+      flower.remove();
+    }, duration * 1000);
+  }
+  setInterval(createFlower, 300);
+  
+
+document.addEventListener("DOMContentLoaded", () => {
+  const headings = document.querySelectorAll(
+    "header h1, section h2"
+  );
+
+  headings.forEach(el => {
+    el.addEventListener("mouseenter", () => {
+      el.classList.remove("rotate-y");
+      el.style.animation = "none";
+
+      void el.offsetHeight;
+
+      el.style.animation = "";
+      el.classList.add("rotate-y");
+    });
+  });
+});
